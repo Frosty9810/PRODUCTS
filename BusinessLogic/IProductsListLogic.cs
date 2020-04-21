405d3263eaa0696cc1a55203d2e7a48abefe6d5a
Josue Cardozo <froilanzambrana_@hotmail.com>
2020-04-21 19:47:16 -0400

    Implementig CRUD in Controllers

--- BusinessLogic/IProductsListLogic.cs
+++ BusinessLogic/IProductsListLogic.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PRODUCTS.DTOModels;
+using PRODUCTS.DataBase.Models;
 
 namespace PRODUCTS.BusinessLogic
 {
     public interface IProductsListLogic
     {
         public List<ProductDTO> GetListProducts();
+        public Product CreateProduct(Product product);
+        public Product deleteProduct(string code);
+        public Product updateProduct(string code, string name, string type, int stock);
     }
 }
--- PRODUCTS/Controllers/ProductListController.cs
+++ PRODUCTS/Controllers/ProductListController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PRODUCTS.DTOModels;
 using PRODUCTS.BusinessLogic;
+using PRODUCTS.DataBase.Models;
+using PRODUCTS.DataBase;
 
 
 namespace PRODUCTS.Controllers
@@ -14,10 +16,12 @@ namespace PRODUCTS.Controllers
     public class ProductListController : ControllerBase
     {
         private readonly IProductsListLogic _productLisLogic;
-        public ProductListController(IProductsListLogic productListLogic)
+         private readonly IProductsDB  _productTableDB;
+        public ProductListController(IProductsListLogic productListLogic, IProductsDB productTableDB)
         {
 
             _productLisLogic = productListLogic;
+            _productTableDB = productTableDB;
 
         }
 
@@ -28,6 +32,24 @@ namespace PRODUCTS.Controllers
             //DEPENDENCY INJECTION.
 
             return _productLisLogic.GetListProducts();
+        }
+
+        [HttpPost]
+        public Product Post ([FromBody] Product product)
+        {       
+            return _productLisLogic.CreateProduct(product);
+        }
+
+         [HttpPut("{id}")]
+        public Product Put(string id,[FromBody] Product product)
+        {
+            return _productLisLogic.updateProduct(id, product.Name, product.Type, product.Stock );
+        }
+
+        [HttpDelete("{id}")]
+        public Product Delete(string id)
+        {
+            return _productLisLogic.deleteProduct(id);
         }
 
     }
